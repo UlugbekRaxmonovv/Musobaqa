b866b558a6efb5fa82b25341518734983c437c79
Ulug'bek <rahmonovulugbek20042004@gmail.com>
2025-07-15 22:04:37 +0500

    Login update

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -4,7 +4,8 @@ import { Link, useNavigate } from "react-router-dom";
 import { LockOutlined } from "@ant-design/icons";
 import toast from "react-hot-toast";
 import axios from "../../api/index";
-import { MdEmail } from 'react-icons/md'
+import { MdEmail } from "react-icons/md";
+import { FaChevronLeft } from "react-icons/fa6";
 
 const Login = () => {
   const navigate = useNavigate();
@@ -19,8 +20,8 @@ const Login = () => {
       });
 
       if (response.data?.accessToken) {
-        localStorage.setItem("x-auth-token",response.data?.accessToken )
-        localStorage.setItem('user',response.data?.user?.email)
+        localStorage.setItem("x-auth-token", response.data?.accessToken);
+        localStorage.setItem("user", response.data?.user?.email);
         toast.success("Muvaffaqiyatli kirildi");
         console.log(response.data?.accessToken);
         navigate("/dashboard/statestika");
@@ -41,58 +42,120 @@ const Login = () => {
   };
 
   return (
-    <div className="w-full min-h-screen bg-[#fff] flex justify-center items-center p-4">
-      <div className="w-full max-w-[400px] bg-white p-10 rounded-xl shadow-2xl">
-        <Link to="/" className="block text-center mb-8">
-          <h2 className="text-4xl font-bold text-gray-800">Tizimga kirish</h2>
-        </Link>
-        <Form
-          name="basic"
-          layout="vertical"
-          initialValues={{ remember: true }}
-          onFinish={onFinish}
-          onFinishFailed={onFinishFailed}
-          autoComplete="off"
-          className="space-y-4"
-        >
-          <Form.Item
-            name="email"
-            rules={[{ required: true, message: "Username kiriting!" }]}
-          >
-            <Input
-              placeholder="Username"
-              className="h-12 text-lg"
-              prefix={<MdEmail className="text-gray-400" />}
-            />
-          </Form.Item>
-
-          <Form.Item
-            name="password"
-            rules={[{ required: true, message: "Parolni kiriting!" }]}
-          >
-            <Input.Password
-              placeholder="Parol"
-              className="h-12 text-lg"
-              prefix={<LockOutlined className="text-gray-400" />}
-            />
-          </Form.Item>
-
-          <Form.Item>
-            <Button
-              type="primary"
-              style={{ width: "100%" }}
-              loading={loading}
-              htmlType="submit"
-              className="w-full h-12 text-lg bg-[#1C59F9] border-0 hover:from-blue-600 hover:to-purple-700"
-            >
-              {loading ? "Kirish..." : "Kirish"}
-            </Button>
-          </Form.Item>
-        </Form>
+    <>
+      <div className="flex  items-center px-4 gap-2 cursor-pointer">
+        <FaChevronLeft className=" text-[12px] text-gray-600" />
+        <p className="text-center text-[16px] text-gray-600">
+          Вернуться на главную
+        </p>
       </div>
-    </div>
+      <div className="min-h-screen flex flex-col items-center justify-center bg-[#fff] px-4">
+        <div className="w-full max-w-[320px] bg-white rounded-xl shadow-2xl">
+          <div className="flex items-center justify-center border-b border-gray-100 py-4 bg-white rounded-t-lg">
+            <div className="flex items-center">
+              <div className="w-6 h-6 relative rounded-full overflow-hidden rotate-45">
+                <div className="absolute inset-0 w-1/2 h-full bg-gradient-to-br from-cyan-400 to-blue-500"></div>
+                <div className="absolute inset-0 w-1/2 h-full left-1/2 bg-gradient-to-bl from-lime-400 to-green-500"></div>
+              </div>
+              <span className="ml-2 text-xl font-bold text-gray-900">
+                Сравни
+              </span>
+            </div>
+          </div>
+
+          <div className="px-10 py-4">
+            <Link to="/" className="block text-center mb-6">
+              <h2 className="text-2xl font-bold text-center text-gray-900">
+                Вход и регистрация
+              </h2>
+              <p className="text-center text-[12px] text-gray-600">
+                Введите ваш номер телефона. На него мы отправим код
+                подтверждения
+              </p>
+            </Link>
+
+            <Form
+              name="basic"
+              layout="vertical"
+              initialValues={{ remember: true }}
+              onFinish={onFinish}
+              onFinishFailed={onFinishFailed}
+              autoComplete="off"
+              className="space-y-4"
+            >
+              <Form.Item
+                name="email"
+                rules={[{ required: true, message: "Введите номер телефона!" }]}
+              >
+                <Input placeholder="Телефон" className="h-12 text-lg" />
+              </Form.Item>
+
+              <Form.Item>
+                <Button
+                  type="black"
+                  style={{ width: "100%" }}
+                  loading={loading}
+                  htmlType="submit"
+                  className="w-full h-12 text-lg bg-[#000] text-white rounded-md"
+                >
+                  {loading ? "" : "Войти"}
+                </Button>
+                <div className="flex justify-center space-x-4 py-3 px-4 mt-2">
+                  <button className="p-2 bg-gray-100 hover:bg-gray-200 rounded-full transition">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      className="w-5 h-5 fill-blue-600"
+                    >
+                      <path d="M15.073 2H8.938C3.332 2 2 3.333 2 8.927v6.136C2 20.667 3.323 22 8.927 22h6.136C20.667 22 22 20.677 22 15.073V8.938C22 3.332 20.677 2 15.073 2z" />
+                    </svg>
+                  </button>
+
+                  <button className="p-2 bg-gray-100 hover:bg-gray-200 rounded-full transition">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      className="w-5 h-5"
+                      fill="#EA4335"
+                    >
+                      <path d="M12.24 10.285V14.4h6.806c-.275 1.765-2.056 5.174-6.806 5.174-4.095 0-7.439-3.389-7.439-7.574s3.345-7.574 7.439-7.574c2.33 0 3.891.989 4.785 1.849l3.254-3.138C18.189 1.186 15.479 0 12.24 0c-6.635 0-12 5.365-12 12s5.365 12 12 12c6.926 0 11.52-4.869 11.52-11.726 0-.788-.085-1.39-.189-1.989H12.24z" />
+                    </svg>
+                  </button>
+
+                  <button className="p-2 bg-gray-100 hover:bg-gray-200 rounded-full transition">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      className="w-5 h-5"
+                      fill="#000"
+                    >
+                      <path d="M18.71 19.5C17.88 20.74 17 21.95 15.66 21.97C14.32 22 13.89 21.18 12.37 21.18C10.84 21.18 10.37 21.95 9.09997 22C7.78997 22.05 6.79997 20.68 5.95997 19.47C4.24997 17 2.93997 12.45 4.69997 9.39C5.56997 7.87 7.12997 6.91 8.81997 6.88C10.1 6.86 11.32 7.75 12.11 7.75C12.89 7.75 14.37 6.68 15.92 6.84C16.57 6.87 18.39 7.1 19.56 8.82C19.47 8.88 17.39 10.1 17.41 12.63C17.44 15.65 20.06 16.66 20.09 16.67C20.06 16.74 19.67 18.11 18.71 19.5ZM13 3.5C13.73 2.67 14.94 2.04 15.94 2C16.07 3.17 15.6 4.35 14.9 5.19C14.21 6.04 13.07 6.7 11.95 6.61C11.8 5.46 12.36 4.26 13 3.5Z" />
+                    </svg>
+                  </button>
+
+                  <button className="p-2 bg-gray-100 hover:bg-gray-200 rounded-full transition">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      className="w-5 h-5 fill-orange-500"
+                    >
+                      <path d="M12 0C5.373 0 0 5.373 0 12s5.373 12 12 12 12-5.373 12-12S18.627 0 12 0zm5.091 17.415c-.184.184-.48.184-.664 0l-3.427-3.427c-.184-.184-.48-.184-.664 0l-3.427 3.427c-.184.184-.48.184-.664 0l-.664-.664c-.184-.184-.184-.48 0-.664l3.427-3.427c-.184-.184-.184-.48 0-.664l-3.427-3.427c-.184-.184-.184-.48 0-.664l.664-.664c.184-.184.48-.184.664 0l3.427 3.427c.184.184.48.184.664 0l3.427-3.427c.184-.184.48-.184.664 0l.664.664c.184.184.184.48 0 .664L14.427 12c.184.184.184.48 0 .664l3.427 3.427c.184.184.184.48 0 .664l-.664.664z" />
+                    </svg>
+                  </button>
+                </div>
+              </Form.Item>
+            </Form>
+          </div>
+        </div>
+
+        <div className="mt-6">
+          <p className="text-blue-500 cursor-pointer text-center">
+            Войти по почте
+          </p>
+        </div>
+      </div>
+    </>
   );
 };
 
 export default Login;
-
